08c51fb2cc03b6ab37baa119a1ef61fd66e96297
v1x1e <github@victoriahchang.com>
2019-08-23 16:46:04 -1000

    Remove comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,59 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Base',
+    description: 'The base foundation for a web application.',
+    author: '@victoriahchang',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
+    'gatsby-transformer-sharp',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: 'images',
+        path: `${__dirname}/src/images/`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: '#222222',
+        display: 'minimal-ui',
+        icon: 'src/images/logo.png', // This path is relative to the root of the site.
+        name: 'base',
+        short_name: 'starter',
+        start_url: '/',
+        theme_color: '#FFFFFF',
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        anonymize: true,
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+        head: true,
+        pageTransitionDelay: 0,
+        respectDNT: true,
+        trackingId: '',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        trackingIds: '',
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+          head: true,
+          respectDNT: true,
+        },
+      },
+    },
   ],
-}
+};
