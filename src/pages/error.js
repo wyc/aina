f577a51aa09155983f1aeb1896f757c3bc30429b
v1x1e <github@victoriahchang.com>
2019-08-23 20:12:34 -1000

    Update Error page class name

--- src/pages/error.js
+++ src/pages/error.js
@@ -3,11 +3,11 @@ import React from 'react';
 import Layout from '../components/Layout/Layout';
 import Metadata from '../components/Metadata/Metadata';
 
-const Error = () => (
+const ErrorPage = () => (
   <Layout>
     <Metadata title='Error 404' />
     Error 404
   </Layout>
 );
 
-export default Error;
+export default ErrorPage;
