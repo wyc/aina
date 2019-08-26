d896d43adf199bdc56757da56b9475bcd97fd77a
v1x1e <github@victoriahchang.com>
2019-08-25 14:48:36 -1000

    Update ESLint and Prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,41 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        jest: true,
-        node: true,
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:prettier/recommended',
-        'plugin:react/recommended',
-        'prettier',
-        'prettier/react',
-    ],
-    globals: {
-        __PATH_PREFIX__: true,
+    ecmaVersion: 9,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'no-multiple-empty-lines': ['error', { max: 2, maxBOF: 1, maxEOF: 1 }],
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    quotes: [2, 'single', { avoidEscape: true }],
+    'react/sort-prop-types': ['error', { sortShapeProp: true }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    parser: 'babel-eslint',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 9,
-        sourceType: 'module',
-    },
-    plugins: [
-        'react',
-        'react-hooks',
-        'prettier'
-    ],
-    rules: {
-        'no-multiple-empty-lines': ['error', { 'max': 2, 'maxBOF': 1, 'maxEOF': 1 }],
-        'prettier/prettier': ['error', { singleQuote: true }],
-        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-        'react-hooks/exhaustive-deps': 'warn',
-        'react-hooks/rules-of-hooks': 'error',
-        'quotes': [2, 'single', { avoidEscape: true }],
-        'react/sort-prop-types': ['error', { 'sortShapeProp': true }]
-    },
-    settings: {
-        react: {
-            version: 'detect'
-        }
-    }
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  'endOfLine': 'lf',
-  'jsxSingleQuote': true,
-  'singleQuote': true,
-  'tabWidth': 2,
-  'trailingComma': 'es5',
-}
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+};
